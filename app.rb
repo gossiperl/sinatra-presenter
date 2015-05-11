ed92f13023c6fc013b716e5be13d1b29d6962db0
radekg <radek@gruchalski.com>
2015-05-11 19:29:23 +0200

    Add logging, add restoring state from opened Keynote. Better handling of next/prev actions - ensure presentation always goes to slideshow, control buttons appearance depending on the slide number. Fixes #1, fixes #2, fixes #4, closes #3 as irrelevant.

--- app.rb
+++ app.rb
@@ -3,10 +3,15 @@ require 'sinatra-websocket'
 require 'json'
 require 'erubis'
 require 'securerandom'
+require 'logger'
+
+logger = Logger.new(STDOUT)
+logger.level = Logger::WARN
 
 set :server, 'thin'
 set :sockets, []
 set :bind, '0.0.0.0'
+set :port, 4567
 
 get '/' do
   if !request.websocket?
@@ -15,7 +20,7 @@ get '/' do
     request.websocket do |ws|
       ws.onopen do
         settings.sockets << ws
-        ws.send( JSON.dump( list_presos ) )
+        ws.send( JSON.dump( get_state ) )
       end
       ws.onmessage do |msg|
         begin
@@ -52,58 +57,118 @@ end
 
 private
 
-def list_presos
-  { :response => "presos", :presos => Dir["#{File.expand_path(File.dirname(__FILE__))}/presos/*.key"] }
+def get_state
+  keynote_state = get_keynote_state
+  keynote_state_file = { :name => nil, :slide => nil, :total => nil }
+  if keynote_state[:cmd_state] == :success
+    if keynote_state[:state] =~ /ok:.*/
+      _, file, slide, total = keynote_state[:state].split(":")
+      keynote_state_file = { :name => file, :slide => slide, :total => total }
+      logger.info "Keynote state valid: #{keynote_state_file}"
+    else
+      logger.info "Keynote state: #{keynote_state[:state]}."
+    end
+  else
+    logger.info "Keynote state failed: #{keynote_state[:error]}."
+  end
+  files = Dir["#{File.expand_path(File.dirname(__FILE__))}/presos/*.key"].map {|s|
+    base_name = File.basename(s)
+    if base_name == keynote_state_file[:name]
+      { :path => s, :name => base_name, :r => true, :slide => keynote_state_file[:slide], :total => keynote_state_file[:total] }
+    else
+      { :path => s, :name => base_name, :r => false }
+    end
+  }
+  logger.info "Returning initial state information."
+  { :response => "state", :presos => files }
 end
 
-def execute_osa_start_command input
-  template = Erubis::Eruby.new(File.read("#{File.expand_path(File.dirname(__FILE__))}/commands/start.applescript.erb"))
-  template_result = template.result(:file_path => input["document"])
+def get_keynote_state
+  logger.info "Attempting to execute Keynote state command..."
+  template = Erubis::Eruby.new(File.read("#{File.expand_path(File.dirname(__FILE__))}/commands/keynote_state.applescript.erb"))
+  template_result = template.result
   file_name = "/tmp/#{SecureRandom.uuid().to_s}.applescript"
   File.write(file_name, template_result.to_s)
   cmdresult = `cat #{file_name} | $(which osascript)`.chomp
   File.delete(file_name)
-  if $?.exitstatus == 0
-    { :response => "opened", :document => input["document"], :slides => cmdresult }
+  code = $?.exitstatus
+  if code == 0
+    logger.info "Keynote state accessed successfully."
+    { :cmd_state => :success, :state => cmdresult }
   else
-    { :response => "open_failed", :document => input["document"], :error => cmdresult }
+    logger.error "Could not load Keynote state. Reason: #{cmdresult}, error code: #{code}"
+    { :cmd_state => :failed, :error => cmdresult, :code => code }
+  end
+end
+
+def execute_osa_start_command input
+  logger.info "Attempting to execute presentation start command..."
+  template = Erubis::Eruby.new(File.read("#{File.expand_path(File.dirname(__FILE__))}/commands/start.applescript.erb"))
+  template_result = template.result(:file_path => input["path"])
+  file_name = "/tmp/#{SecureRandom.uuid().to_s}.applescript"
+  File.write(file_name, template_result.to_s)
+  cmdresult = `cat #{file_name} | $(which osascript)`.chomp
+  File.delete(file_name)
+
+  code = $?.exitstatus
+  if code == 0
+    logger.info "Presentation opened successfully."
+    { :response => "opened", :path => input["path"], :name => input["name"], :slides => cmdresult }
+  else
+    logger.error "Could not open the presentation. Reason: #{cmdresult}, exit code #{code}"
+    { :response => :failed, :command => input["command"], :error => cmdresult, :code => code }
   end
 end
 
 def execute_osa_next_command input
+  logger.info "Attempting progressing to the next slide. (current slide: #{input["current"]})"
   template = Erubis::Eruby.new(File.read("#{File.expand_path(File.dirname(__FILE__))}/commands/next.applescript.erb"))
-  template_result = template.result
+  template_result = template.result( :current => input["current"] )
   file_name = "/tmp/#{SecureRandom.uuid().to_s}.applescript"
   File.write(file_name, template_result.to_s)
   cmdresult = `cat #{file_name} | $(which osascript)`.chomp
   File.delete(file_name)
-  if $?.exitstatus == 0
+
+  code = $?.exitstatus
+  if code == 0
     { :response => "changed", :document => input["document"], :slide => cmdresult }
   else
-    { :response => "change_failed", :document => input["document"], :error => cmdresult }
+    logger.error "Could not progress to the next slide. Reason: #{cmdresult}, exit code #{code}"
+    { :response => :failed, :command => input["command"], :error => cmdresult, :code => code }
   end
 end
 
 def execute_osa_prev_command input
+  logger.info "Attempting going back to the previous slide (current slide: #{input["current"]})."
   template = Erubis::Eruby.new(File.read("#{File.expand_path(File.dirname(__FILE__))}/commands/prev.applescript.erb"))
-  template_result = template.result
+  template_result = template.result( :current => input["current"] )
   file_name = "/tmp/#{SecureRandom.uuid().to_s}.applescript"
   File.write(file_name, template_result.to_s)
   cmdresult = `cat #{file_name} | $(which osascript)`.chomp
   File.delete(file_name)
-  if $?.exitstatus == 0
+  
+  code = $?.exitstatus
+  if code == 0
     { :response => "changed", :document => input["document"], :slide => cmdresult }
   else
-    { :response => "change_failed", :document => input["document"], :error => cmdresult }
+    logger.error "Could not go back to the previous slide. Reason: #{cmdresult}, exit code #{code}"
+    { :response => :failed, :command => input["command"], :error => cmdresult, :code => code }
   end
 end
 
 def execute_osa_stop_command input
+  logger.info "Attempting to stop Keynote...."
   template = Erubis::Eruby.new(File.read("#{File.expand_path(File.dirname(__FILE__))}/commands/stop.applescript.erb"))
   template_result = template.result
   file_name = "/tmp/#{SecureRandom.uuid().to_s}.applescript"
   File.write(file_name, template_result.to_s)
   cmdresult = `cat #{file_name} | $(which osascript)`.chomp
   File.delete(file_name)
-  { :response => "quit", :document => input["document"] }
+  code = $?.exitstatus
+  if code == 0
+    { :response => "quit" }
+  else
+    logger.error "Could not stop Keynote. Reason: #{cmdresult}, exit code #{code}"
+    { :response => :failed, :command => input["command"], :error => cmdresult, :code => code }
+  end 
 end
